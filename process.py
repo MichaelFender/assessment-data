7578b9ddc30aa8127b299e89ca15c887b4d2cecc
MichaelFeneder <michael.a.fender@outlook.com>
2021-11-12 15:55:33 -0600

    explained what each line is doing

--- process.py
+++ process.py
@@ -5,7 +5,7 @@ def sales_reports(log_file): #passes "log_file" into the new function "sales_rep
     for line in log_file: #loop over each line in the txt file
         line = line.rstrip() #rstrip removes trailing characters
         day = line[0:3] #selects the line in a particular day
-        if day == "Mon": #singles out day if = "Mon"
+        if day == "Tue": #singles out day if = "Tue"
             print(line) #will print out Monday's reports to the terminal
 
 
