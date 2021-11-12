11e806e177943e602bbad639cd2798e4fc85801a
MichaelFeneder <michael.a.fender@outlook.com>
2021-11-12 15:54:14 -0600

    explained what each line is doing

--- process.py
+++ process.py
@@ -1,12 +1,12 @@
-log_file = open("um-server-01.txt")
+log_file = open("um-server-01.txt") #allows this .py to use the data in the .txt file
 
 
-def sales_reports(log_file):
-    for line in log_file:
-        line = line.rstrip()
-        day = line[0:3]
-        if day == "Tue":
-            print(line)
+def sales_reports(log_file): #passes "log_file" into the new function "sales_reports"
+    for line in log_file: #loop over each line in the txt file
+        line = line.rstrip() #rstrip removes trailing characters
+        day = line[0:3] #selects the line in a particular day
+        if day == "Mon": #singles out day if = "Mon"
+            print(line) #will print out Monday's reports to the terminal
 
 
-sales_reports(log_file)
+sales_reports(log_file)#will show the full result from the function
